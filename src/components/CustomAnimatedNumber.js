616b168c0eb265c0b2504818625d1b5d424c9179
GouravShDev <gourav0sharma1@gmail.com>
2021-07-09 23:35:02 +0530

    fixed animated number bug

--- src/components/CustomAnimatedNumber.js
+++ src/components/CustomAnimatedNumber.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AnimatedNumber from "react-animated-numbers"
 import './CustomAnimatedNumber.css'
 
 
+
+
 export default function CustomAnimatedNumber({ caseType, textColor, number }) {
+    const [windowWidth, setWindowWidth] = useState(window.innerHeight);
+
+    useEffect(() => {
+        window.addEventListener('resize', () => { setWindowWidth(window.innerWidth) });
+    })
+
     return (
         <div className='anim-number-container'>
             <h2 className="font-1" style={{ color: textColor }}>
                 {caseType} :
             </h2>
             <AnimatedNumber
+                key={windowWidth}
                 animateToNumber={number}
                 fontStyle={{ fontFamily: "Nunito", fontSize: 'var(--title-size)' }}
                 includeComma
