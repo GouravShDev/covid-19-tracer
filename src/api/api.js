ebee660c387d7868e706abc88d75e8cdb61625fe
Shivanshu10 <smishra_be20@thapar.edu>
2021-07-09 17:29:37 +0530

    fixed backend errors

--- src/api/api.js
+++ src/api/api.js
@@ -1,57 +1,40 @@
 import country_codes from "./country_codes";
-
 class API {
-    constructor() {
+    async init() {
         this.api='https://api.covid19api.dev';
-        this.confirmed_req = new XMLHttpRequest();
-        this.recovered_req = new XMLHttpRequest();
-        this.deaths_req = new XMLHttpRequest();
-        this.url_confirmed=this.api+'/time_series_confirmed_global';
-        this.url_recovered=this.api+'/time_series_recovered_global';
-        this.url_deaths=this.api+'/time_series_deaths_global';
         const url=this.api+'/token';
-        this.key_req = new XMLHttpRequest();
-        var params=JSON.stringify({"username": "public_user", "password": "public_pass"});
-        this.key_req.open("POST", url);
-        this.key_req.setRequestHeader('Content-Type', 'application/json');
-        this.key_req.send(params);
-        this.key_req.onreadystatechange = (e) => {
-            if (this.key_req.readyState===4) {
-                this.key='Bearer '+JSON.parse(this.key_req.responseText).Document;
-                //console.log(Http.responseText);
-            }
+        var params={
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({"username": "public_user", "password": "public_pass"})
         }
-        //this.key='Bearer '+key; 
+        var data=await this.makeRequest(url, params);
+        var key="Bearer "+data["Document"];
+        return key;
     }
-    async getData(country_abrv, date, fn) {
-        var country=country_codes[country_abrv];
-        this.confirmed_req.open("GET", this.url_confirmed, true);
-        this.recovered_req.open("GET", this.url_recovered, true);
-        this.deaths_req.open("GET", this.url_deaths, true);
-        this.confirmed_req.setRequestHeader('Content-Type', 'application/json');
-        this.recovered_req.setRequestHeader('Content-Type', 'application/json');
-        this.deaths_req.setRequestHeader('Content-Type', 'application/json');
-        this.confirmed_req.setRequestHeader('Authorization', this.key);
-        this.recovered_req.setRequestHeader('Authorization', this.key);
-        this.deaths_req.setRequestHeader('Authorization', this.key);
-        this.confirmed_req.send();
-        this.recovered_req.send();
-        this.deaths_req.send();
-        this.confirmed_req.onreadystatechange = (e) => {
-            if (this.confirmed_req.readyState===4) {
-                var data_arr=JSON.parse(this.confirmed_req.responseText).Document;
-                var data;
-                for (data in data_arr) {
-                    //console.log(data_arr[data]);
-                    if (data_arr[data].country_region === country) {
-                        fn(data_arr[data][date], 0);
-                    }
-                }
+    async makeRequest(url, param) {
+        var resp=await fetch(url, param);
+        //console.log(resp);
+        return resp.json();
+    }
+    async getData(type, country_abrv, date, fn, key) {
+        country=country_codes[country_abrv]
+        const url=this.api+'/time_series_'+type+'_global';
+        //console.log(key);
+        key=await key;
+        //console.log(key);
+        var params={
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": key
             }
-        }
-        this.recovered_req.onreadystatechange = (e) => {
-            if (this.recovered_req.readyState===4) {
-                var data_arr=JSON.parse(this.recovered_req.responseText).Document;
+        };
+        this.makeRequest(url, params).then(d => {
+                console.log(d);
+                var data_arr=d.Document;
                 var data;
                 for (data in data_arr) {
                     //console.log(data_arr[data]);
@@ -60,22 +43,16 @@ class API {
                     }
                 }
             }
-        }
-        this.deaths_req.onreadystatechange = (e) => {
-            if (this.deaths_req.readyState===4) {
-                var data_arr=JSON.parse(this.deaths_req.responseText).Document;
-                var data;
-                for (data in data_arr) {
-                    //console.log(data_arr[data]);
-                    if (data_arr[data].country_region === country) {
-                        fn(data_arr[data][date], 1);
-                    }
-                }
-            }
-        }
+        );
     }
 }
+/*
+fn=(i, j) => {
+    console.log(i);
+    console.log(j);
+}
 
-//var a=new API();
-//console.log(a.getData("recovered", "Afghanistan", "mar232021"));
-export default API;
+var a=new API();
+k=a.init();
+a.getData("recovered", "Afghanistan", "mar232021", fn, k);
+*/
